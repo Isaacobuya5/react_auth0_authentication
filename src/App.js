42306241ce46cff29a79fc4311c6d574ad0f9b89
Isaac Obuya <Szack4477@gmail.com>
2020-05-19 10:24:58 +0300

    add routing for home and profile pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/profile" component={Profile} />
+      </Switch>
     </div>
   );
 }
