08f174c713b7c0b4299d970b9dd390cecb404c20
Isaac Obuya <Szack4477@gmail.com>
2020-05-19 10:09:03 +0300

    setup new project and install all required packages

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+const Home = props => {
+    return (
+        <div>
+            Home page
+        </div>
+    );
+};
+
+Home.propTypes = {
+    
+};
+
+export default Home;
