f5e50220846b501bcef3b6266b32d06e6429383f
Isaac Obuya <Szack4477@gmail.com>
2020-05-22 09:38:31 +0300

    create a server with a public and private endpoint, create auth0 api and configure server to parse JWT, call public and private APIs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useMemo, useEffect} from 'react';
+import React, { useMemo } from 'react';
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import NavBar from "./components/NavBar";
 import Callback from "./components/Callback";
+import Public from "./components/Public";
+import Private from "./components/Private";
 
 import Auth from "./auth/Auth";
 
@@ -21,6 +23,8 @@ function App(props) {
         <Route path="/" render={props => <Home auth={auth} {...props} />} exact/>
         <Route path="/profile" render={props => auth.isAuthenticated() ? (<Profile auth={auth} {...props}/>) : (<Redirect to="/" />) } />
         <Route path="/callback" render={props => <Callback auth={auth} {...props}/>} />
+        <Route path="/public" component={Public} />
+        <Route path="/private" render={props => auth.isAuthenticated() ? (<Private auth={auth} {...props}/>) : (<Redirect to="/" />) } />
         </div>
       </Switch>
     </div>
